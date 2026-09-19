69df130abd3eece6b03f8acae692beb6517adb0c
msbangsh-ui <msbangsh@gmail.com>
2026-09-19 09:05:42 +0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,7 @@
-const CACHE="my-expense-v3";const ASSETS=["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='expense-tracker-v1';
+const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('fetch',e=>{
+  const u=new URL(e.request.url);
+  if(u.origin===location.origin) e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+});
